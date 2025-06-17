602a228296d3504381ff4b8f88db95fcac2a4942
rayhantmt <rayhantmt@gmail.com>
2025-06-18 01:46:41 +0600

    added home container

--- lib/app/modules/home/home_view.dart
+++ lib/app/modules/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:damaged303/app/utils/app_images.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -27,6 +28,92 @@ class HomeView extends StatelessWidget {
             icon: Icon(Icons.notifications_none_outlined, size: 30),
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: Get.height * 0.2,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: Image.asset(
+                          AppImages.home_container,
+                          fit: BoxFit.cover,
+                          color: Color(0xFF000000).withOpacity(0.6),
+                          colorBlendMode: BlendMode.darken,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 20,
+                          right: 20,
+                          top: 10,
+                          bottom: 10,
+                        ),
+                        child: Column(
+                          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Breaking News',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 24,
+                                color: Color(0xFFFFFFFF),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Text(
+                              'Company-wide policy update: New flexible guidelines released.',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16,
+                                color: Color(0xFFFFFFFF),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(6),
+                                  color: Color(0xFF013D3B),
+                                ),
+                                width: 120,
+                                height: 40,
+
+                                child: Center(
+                                  child: Text(
+                                    'View Feed',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 16,
+                                      color: Color(0xFFFFFFFF),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/app/utils/app_images.dart
+++ lib/app/utils/app_images.dart
@@ -1,6 +1,6 @@
 class AppImages {
-
-static const String splash = 'assets/images/splash.png';
-static const String google='assets/images/google.png';
-  static const String apple='assets/images/apple.png';
+  static const String splash = 'assets/images/splash.png';
+  static const String google = 'assets/images/google.png';
+  static const String apple = 'assets/images/apple.png';
+  static const String home_container = 'assets/images/home_background.jpg';
 }
