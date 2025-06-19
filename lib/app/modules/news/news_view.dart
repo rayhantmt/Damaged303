f0b8449a4b6650ef226272e5ecde6afe94f784a7
rayhantmt <rayhantmt@gmail.com>
2025-06-20 03:34:34 +0600

    sfag

--- lib/app/modules/news/news_view.dart
+++ lib/app/modules/news/news_view.dart
@@ -1,4 +1,8 @@
+import 'package:damaged303/app/modules/home/home_view.dart';
+import 'package:damaged303/app/modules/main_screen/main_screen_controller.dart';
+import 'package:damaged303/app/modules/main_screen/main_screen_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class NewsView extends StatelessWidget {
   const NewsView({super.key});
@@ -7,6 +11,16 @@ class NewsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () {
+            Get.put<BottomNavController>(BottomNavController()).changeTab(0);
+            Get.back();
+
+            //i want to naviagate to homeview with the bottom navigation bar
+            print('news back');
+          },
+          child: Icon(Icons.arrow_back),
+        ),
         title: Text(
           'News Feed',
           style: TextStyle(
@@ -16,10 +30,33 @@ class NewsView extends StatelessWidget {
           ),
         ),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.notifications_none_outlined, size: 30),
+          ),
+        ],
       ),
       body: Column(
         children: [
-          SingleChildScrollView(child: Center(child: Text("News Screen"))),
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Container(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.search),
+                    hintText: 'Search News',
+                  ),
+                ),
+                height: 45,
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Color(0xFFB0C3C2)),
+                  borderRadius: BorderRadius.circular(6),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
