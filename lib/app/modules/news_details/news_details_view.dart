dad69b6295a0da625de935420ccbf593653bf720
rayhantmt <rayhantmt@gmail.com>
2025-06-21 04:26:57 +0600

    news details view done

--- lib/app/modules/news_details/news_details_view.dart
+++ lib/app/modules/news_details/news_details_view.dart
@@ -1,4 +1,6 @@
+import 'package:damaged303/app/utils/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class NewsDetailsView extends StatelessWidget {
   const NewsDetailsView({
@@ -25,7 +27,101 @@ class NewsDetailsView extends StatelessWidget {
         ),
       ),
 
-      body: Center(child: Text(description)),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Container(
+                  height: 40,
+                  width: Get.width * .25,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: AppColors.primarycolor,
+                  ),
+                  child: Center(
+                    child: Text(
+                      tags.first,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                        color: Color(0xFF050505),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 2),
+                Container(
+                  height: 40,
+                  width: Get.width * .25,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Color(0xFFE6ECEB), width: 1),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Center(
+                    child: Text(
+                      tags.last,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                        color: Color(0xFF050505),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Text(
+              headline,
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 18,
+                color: Color(0xFF1B1E28),
+              ),
+            ),
+            Text(
+              description,
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                color: Color(0xFF7D848D),
+                fontSize: 16,
+              ),
+            ),
+            Expanded(child: SizedBox()),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    height: 40,
+                    width: 115,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: AppColors.primarycolor,
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Read more',
+                        style: TextStyle(
+                          color: Color(0xFFE6E6E6),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
     );
   }
 }
