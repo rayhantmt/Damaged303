e31cf19c1c3ea33baf76e006bee20f5f8c9e30c4
rayhantmt <rayhantmt@gmail.com>
2025-06-21 03:39:59 +0600

    done news view

--- lib/app/modules/news/news_model.dart
+++ lib/app/modules/news/news_model.dart
@@ -25,3 +25,33 @@ class newsdetailsmodel {
     required this.description,
   });
 }
+
+class newsdescritiondetail {
+  final List<newsdetailsmodel> newsdescriptions = [
+    newsdetailsmodel(
+      tags: ['Strategy', 'SHRM'],
+      headline: 'CHROS Driving Business Transfomutioo at Sumtfiit',
+      description:
+          'Internal mobility refers to the movement of employees within an organization, whether through promotions, lateral moves, project-based assignments, or role changes across departments. It plays a critical role in talent retention, employee engagement, and organizational agility. By prioritizing internal mobility, companies can unlock hidden talent, reduce hiring costs, and build a more adaptable workforce. However, successful implementation requires transparent career paths, skills-based development programs, and supportive leadership. As the future of work demands faster adaptation, internal mobility is no longer optional—it’s a strategic necessity.   ',
+    ),
+    newsdetailsmodel(
+      tags: ['Cmplance', 'HR Drive'],
+      headline: 'California Workplace Violence Rules Now in Effect',
+      description:
+          'New California workplace violence prevention rules took effect July 1, 2024, requiring most employers to implement comprehensive safety plans. These regulations mandate risk assessments, employee training, and incident reporting procedures to address growing concerns about workplace violence. Employers must identify hazards, develop prevention strategies, and provide clear response protocols. The rules apply to nearly all industries, with stricter requirements for high-risk sectors like healthcare and retail. Non-compliance may result in penalties. California follows federal OSHA guidelines while expanding protections, reflecting rising national attention on employee safety. Experts recommend immediate action to update policies, train staff, and document compliance efforts to avoid fines and foster safer work environments.',
+    ),
+
+    newsdetailsmodel(
+      tags: ['Talent', 'Forbes'],
+      headline: 'A Deep Dive into internal Mobility',
+      description:
+          'Internal mobility empowers employees to grow within an organization through promotions, lateral moves, or cross-departmental transfers. It boosts retention, engagement, and agility while reducing hiring costs. By leveraging existing talent, companies foster career development and adaptability. Success depends on transparent career paths, skills training, and leadership support. As workplaces evolve, internal mobility shifts from perk to necessity—helping businesses stay competitive and employees thrive. A well-structured program aligns organizational goals with individual aspirations, creating a dynamic, future-ready workforce.',
+    ),
+    newsdetailsmodel(
+      tags: ['Talent', 'Forbes'],
+      headline: 'California Workplace Violence Rules Now in Effect',
+      description:
+          'Internal mobility empowers employees to grow within an organization through promotions, lateral moves, or cross-departmental transfers. It boosts retention, engagement, and agility while reducing hiring costs. By leveraging existing talent, companies foster career development and adaptability. Success depends on transparent career paths, skills training, and leadership support. As workplaces evolve, internal mobility shifts from perk to necessity—helping businesses stay competitive and employees thrive. A well-structured program aligns organizational goals with individual aspirations, creating a dynamic, future-ready workforce.',
+    ),
+  ];
+}
--- lib/app/modules/news/news_view.dart
+++ lib/app/modules/news/news_view.dart
@@ -1,6 +1,7 @@
-import 'package:damaged303/app/modules/home/home_view.dart';
 import 'package:damaged303/app/modules/main_screen/main_screen_controller.dart';
-import 'package:damaged303/app/modules/main_screen/main_screen_view.dart';
+
+import 'package:damaged303/app/modules/news/news_model.dart';
+import 'package:damaged303/app/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,6 +10,7 @@ class NewsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final newsList = newsdescritiondetail().newsdescriptions;
     return Scaffold(
       appBar: AppBar(
         leading: GestureDetector(
@@ -16,7 +18,6 @@ class NewsView extends StatelessWidget {
             Get.put<BottomNavController>(BottomNavController()).changeTab(0);
             Get.back();
 
-            //i want to naviagate to homeview with the bottom navigation bar
             print('news back');
           },
           child: Icon(Icons.arrow_back),
@@ -56,19 +57,112 @@ class NewsView extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 20),
 
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Container(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Select a category',
+                      ),
+                    ),
+                    height: 45,
+                    width: Get.width * 0.5,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Color(0xFFB0C3C2)),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
             Padding(
               padding: const EdgeInsets.only(left: 20, right: 20),
-              child: Container(
-                child: TextFormField(
-                  decoration: InputDecoration(hintText: 'Select a category'),
-                ),
-                height: 45,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1, color: Color(0xFFB0C3C2)),
-                  borderRadius: BorderRadius.circular(6),
-                ),
+              child: ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: newsList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final tags = newsList[index].tags;
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 40,
+                            width: Get.width * .25,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: AppColors.primarycolor,
+                            ),
+                            child: Center(
+                              child: Text(
+                                tags.first,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                  color: Color(0xFF050505),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 2),
+                          Container(
+                            height: 40,
+                            width: Get.width * .25,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Color(0xFFE6ECEB),
+                                width: 1,
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Center(
+                              child: Text(
+                                tags.last,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                  color: Color(0xFF050505),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        newsList[index].headline,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                          color: Color(0xFF1B1E28),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 3,
+                          newsList[index].description,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                            color: Color(0xFF7D848D),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                    ],
+                  );
+                },
               ),
             ),
           ],
