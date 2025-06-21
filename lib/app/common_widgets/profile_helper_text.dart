2117d797303e4dd5e621bf542c64c26f1f8bd0f4
rayhantmt <rayhantmt@gmail.com>
2025-06-22 04:58:55 +0600

    profile log out section

--- lib/app/common_widgets/profile_helper_text.dart
+++ lib/app/common_widgets/profile_helper_text.dart
@@ -4,6 +4,7 @@ class ProfileHelp extends StatelessWidget {
   const ProfileHelp({super.key, required this.icon, required this.text});
   final IconData icon;
   final String text;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
--- lib/app/modules/profile/porfile_view.dart
+++ lib/app/modules/profile/porfile_view.dart
@@ -46,7 +46,32 @@ class PorfileView extends StatelessWidget {
             text: 'Privacy Policy',
           ),
           ProfileHelp(icon: Icons.lock_outline_sharp, text: 'Forgot Password'),
-          ProfileHelp(icon: Icons.logout_outlined, text: 'Log out'),
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.logout_outlined, color: Color(0xffD40606)),
+                      SizedBox(width: 8),
+                      Text(
+                        'Log out',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                          color: Color(0xffD40606),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Icon(Icons.arrow_forward_ios_outlined, size: 16),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
